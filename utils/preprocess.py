abcf7dc83062440582af9dd1088f0e4844e63173
nmarzz <ndmarshall97@gmail.com>
2021-03-08 09:59:42 -0800

    new pre-process

---
+++ utils/preprocess.py
@@ -0,0 +1,11 @@
+from copy import deepcopy
+import pandas as pd
+
+
+def get_daily_return(df, value_col_name="account_value"):
+    df = deepcopy(df)
+    df["daily_return"] = df[value_col_name].pct_change(1)
+    df["date"] = pd.to_datetime(df["date"])
+    df.set_index("date", inplace=True, drop=True)
+    df.index = df.index.tz_localize("UTC")
+    return pd.Series(df["daily_return"], index=df.index)
