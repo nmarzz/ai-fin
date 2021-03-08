b33016fc7bc274cf6c24b5e62be8a4ae3ec32f93
nmarzz <ndmarshall97@gmail.com>
2021-03-08 11:27:26 -0800

    add arg parsing

--- train_on_yahoo.py
+++ train_on_yahoo.py
@@ -115,9 +115,11 @@ ppo_params ={'n_steps': 256,
              'batch_size': 512,
             'gamma': 0.99}
 
-model_params = {args.model: config.__dict__[f"{args.model.upper()}_PARAMS"]}
+model_params = config.__dict__[f"{args.model.upper()}_PARAMS"]
 
 print(model_params)
+print('made it baby')
+
 
 policy_kwargs = {
     "net_arch": [1024, 1024,1024, 1024,  1024],
