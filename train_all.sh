2b219f184470b43506c0e9e2021bcc65560026ba
nmarzz <ndmarshall97@gmail.com>
2021-03-15 16:38:43 -0700

    add bash

--- train_all.sh
+++ train_all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Trains each model ()['ppo','ddpg','a2c','td3','sac']) for the specified number of training steps
-trainsteps = $1
+TRAINSTEPS=$1
 
-for model in ppo ddpg a2c td3 sac
+for MODEL in ppo ddpg a2c td3 sac
 do
   python train_on_yahoo.py\
-   --model $model
-   --train-steps $trainsteps
+  --model $MODEL
+  --train-steps $TRAINSTEPS
 done
