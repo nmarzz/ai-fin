0d0e4acda36c897a12acf438e3b5c1a5b8cbfe59
nmarzz <ndmarshall97@gmail.com>
2021-04-13 18:31:29 -0700

    updates

--- compare_ensemble_strats.py
+++ compare_ensemble_strats.py
@@ -35,16 +35,16 @@ data_dir = 'data'
 # Dow
 # model_paths = ['models/models/a2c_dow29_steps100000_start2000-01-01_end2018-01-01.model','models/models/ddpg_dow29_steps100000_start2000-01-01_end2018-01-01.model','models/models/ppo_dow29_steps100000_start2000-01-01_end2018-01-01.model','models/models/sac_dow29_steps100000_start2000-01-01_end2018-01-01.model','models/models/td3_dow29_steps100000_start2000-01-01_end2018-01-01.model']
 # Dow without a2c
-model_paths = ['models/models/ddpg_dow29_steps100000_start2000-01-01_end2018-01-01.model','models/models/ppo_dow29_steps100000_start2000-01-01_end2018-01-01.model','models/models/sac_dow29_steps100000_start2000-01-01_end2018-01-01.model','models/models/td3_dow29_steps100000_start2000-01-01_end2018-01-01.model']
+# model_paths = ['models/models/ddpg_dow29_steps100000_start2000-01-01_end2018-01-01.model','models/models/ppo_dow29_steps100000_start2000-01-01_end2018-01-01.model','models/models/sac_dow29_steps100000_start2000-01-01_end2018-01-01.model','models/models/td3_dow29_steps100000_start2000-01-01_end2018-01-01.model']
 # Nasdaq
-# model_paths = ['models/models/a2c_nas29_steps1000000_start2005-01-01_end2018-11-28.model','models/models/ddpg_nas29_steps1000000_start2005-01-01_end2018-11-28.model','models/models/ppo_nas29_steps1000000_start2005-01-01_end2018-11-28.model','models/models/sac_nas29_steps1000000_start2005-01-01_end2018-11-28.model','models/models/td3_nas29_steps1000000_start2005-01-01_end2018-11-28.model']
+model_paths = ['models/models/a2c_nas29_steps1000000_start2005-01-01_end2018-11-28.model','models/models/ddpg_nas29_steps1000000_start2005-01-01_end2018-11-28.model','models/models/ppo_nas29_steps1000000_start2005-01-01_end2018-11-28.model','models/models/sac_nas29_steps1000000_start2005-01-01_end2018-11-28.model','models/models/td3_nas29_steps1000000_start2005-01-01_end2018-11-28.model']
 start_date,split_date,data_type ,model = get_model_info_from_path(model_paths)
 
 end_date = '2020-12-31' # Model is tested from split_date to end_date
 
-# end_date = '2018-01-01' # Model is tested from split_date to end_date
-# split_date = '2015-01-01'
-
+end_date = '2018-01-01' # Model is tested from split_date to end_date
+split_date = '2015-01-01'
+print(data_type)
 
 # Get data
 df = get_dataset(data_dir,data_type,split_date,end_date)
@@ -86,7 +86,7 @@ else:
 print('Testing...')
 df_account_value, df_actions = DRLAgent.average_predict(
     model=trained_model,
-    environment = test_gym_env,n_evals = 5)
+    environment = test_gym_env,n_evals = 15)
 
 
 
@@ -125,7 +125,7 @@ else:
 print('Testing...')
 df_account_value, df_actions = DRLAgent.average_predict(
     model=trained_model,
-    environment = test_gym_env,n_evals = 5)
+    environment = test_gym_env,n_evals = 15)
 
 
 df_account_value.to_csv('results/average_ensemble_accounts.csv')
@@ -133,7 +133,7 @@ df_actions.to_csv('results/average_ensemble_actions.csv')
 
 print('Comparing to DJI')
 dji = YahooDownloader(
-            start_date=split_date, end_date=end_date, ticker_list=['^DJI']
+            start_date=split_date, end_date=end_date, ticker_list=['^IXIC'] # Dow = '^DJI', NASDAQ = '^IXIC'
         ).fetch_data()
 dates_rl = matplotlib.dates.date2num(df_account_value['date'])
 dates_base = matplotlib.dates.date2num(dji['date'])
@@ -141,11 +141,12 @@ dates_base = matplotlib.dates.date2num(dji['date'])
 
 init_dji_shares = 1000000/dji['close'][0]
 
-
+start_date = '2005-01-01'
+split_date = '2018-11-28' 
 plt.plot_date(dates_rl,df_account_value['account_value'],'-')
 plt.plot_date(dates_base,dji['close'] * init_dji_shares,'-')
-plt.legend(['bin-avg','avg','dji'])
+plt.legend(['bin-avg','avg','nasdaq'])
 plt.title(f'Ensemble trained from {start_date}-{split_date}')
 plt.ylabel('Account Value')
 plt.xticks(rotation=30)
-plt.savefig(f'imgs/{model}_vs_dji_{split_date}_{end_date}.png')
+plt.savefig(f'imgs/{model}_vs_NASDAQ_{split_date}_{end_date}.png')
